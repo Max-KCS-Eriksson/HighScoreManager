5435150c6211063753e6e09edffe24a81d77d5bd
Max Eriksson <teratoscincus@hotmail.com>
2025-01-18 15:05:41 +0100

    Add Cucumber runner

---
+++ src/test/java/com/maxeriksson/CucumberTest.java
@@ -0,0 +1,13 @@
+package com.maxeriksson;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.maxeriksson.stepdefs")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class CucumberTest {}
