aa02d5800c564d3898ba1488e217c0cc875c3be1
Max Eriksson <teratoscincus@hotmail.com>
2025-01-19 04:58:33 +0100

    Add class to manage high scores

---
+++ src/main/java/com/maxeriksson/domain/HighScoreManager.java
@@ -0,0 +1,21 @@
+package com.maxeriksson.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HighScoreManager {
+
+    List<Integer> scores;
+
+    public HighScoreManager() {
+        this.scores = new ArrayList<>();
+    }
+
+    public HighScoreManager(List<Integer> scores) {
+        this.scores = new ArrayList<>(scores);
+    }
+
+    public List<Integer> getScoresTop(int topNum) {
+        return this.scores;
+    }
+}
